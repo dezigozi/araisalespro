03a99f1e27e39fffcb2f9963937560cfefcb85a3
dezigozi <64128226+dezigozi@users.noreply.github.com>
2026-04-03 19:14:07 +0900

    Fix GAS_WEBAPP_URL to the correct endpoint

--- js/gas-config.js
+++ js/gas-config.js
@@ -3,4 +3,4 @@
  * ARAI_SALES_PRO の Apps Script からデプロイした /exec をここにだけ書く。
  * 各 HTML では他の js より先に <script src="js/gas-config.js"></script> を読み込むこと。
  */
-var GAS_WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbywfK9XT0cvtfSmVGmB5SjRALqU_qrJrRSwcyCPa3pyXYqGgJhRYO-SfQ4mpPwypwgA7g/exec';
+var GAS_WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbwrd5rm_eKQGJgON83RW8qg5H0SkMkqk6Zmrwh-lM62cqG6he9Ugq-7vmN0wXaaj-a3Nw/exec';
